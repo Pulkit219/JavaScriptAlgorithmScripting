965b2762a11547c95dc48af03109a2b19de51fe6
Pulkit219 <ascension1110@gmail.com>
2017-07-29 22:57:56 -0400

    12th comm

---
+++ testing_with_mocha/lib/reverse.js
@@ -0,0 +1,4 @@
+module.exports = function(word)
+{
+	return word.split('').reverse().join('')
+}
---
+++ testing_with_mocha/test/reverse.test.js
@@ -0,0 +1,16 @@
+var reverse = require('../lib/reverse')
+var expect = require('chai').expect
+
+
+describe("#reverse", function (){
+
+		it("reverse the given string", function()
+	{
+		expect(reverse("hello")).to.equal("olleh")
+
+	})
+
+
+
+})
+
